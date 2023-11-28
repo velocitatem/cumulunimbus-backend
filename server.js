b9cac22fe03438fb6840127607312c4ddab86563
velocitatem <daniel@alves.world>
2023-11-28 15:52:35 +0100

    all code

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { Client: ServiceClient} = require('azure-iothub');
+const { Message } = require('azure-iot-common');
+
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(cors());
@@ -10,29 +13,40 @@ app.use(cors());
 
 // azure iot hub connection
 const connectionString = 'HostName=Cumulonimbus.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=70+uDUmL/oVHTnRMVTbLiJpNfeOwPPAoRAIoTHkXlc0=';
+const clientFromConnectionString = require('azure-iot-device-mqtt').clientFromConnectionString;
+// Service client for Azure IoT Hub
+const serviceClient = ServiceClient.fromConnectionString(connectionString);
+
 
 // connect to azure iot hub
 
 
 app.get('/status', (req, res) => {
     // chekc if the client is connected
-    if (client.isOpen()) {
-        res.send('Connected');
+    if (serviceClient) {
+        res.send('connected');
     }
     else {
-        res.send('Not connected');
+        res.send('not connected');
     }
+
 });
 
 
-app.post("/open/:id", (req, res) => {
+app.post("/open/:id", async (req, res) => {
     // just log it for now
     const {state} = req.body;
-    const id = req.params.id;
-    const st = state ? 'open' : 'close';
-    console.log(`Received request to ${st} the door with id ${id}`);
-
-    res.send('OK');
+    const deviceId = req.params.id;
+    const command = state ? 'open' : 'close';
+    // Create a message and send it to the IoT Hub for a specific device
+    const message = new Message(JSON.stringify({ command }));
+    try {
+        await serviceClient.send(deviceId, message);
+        res.send(`Command to ${command} sent to device ${deviceId}`);
+    } catch (err) {
+        console.error('Could not send message: ' + err.message);
+        res.status(500).send('Failed to send message');
+    }
 });
 
 
@@ -45,9 +59,21 @@ app.get('/', (req, res) => {
 
 
 const port = process.env.PORT || 3000;
-const start = (callback) => {
-    callback()
+
+const connectCallback = (err) => {
+    if (err) {
+        console.error('Could not connect: ' + err.message);
+    } else {
+        console.log('Client connected');
+
+    }
 };
+
+
+const start = (callback) => {
+    callback();
+};
+
 
 
 start(() => {
